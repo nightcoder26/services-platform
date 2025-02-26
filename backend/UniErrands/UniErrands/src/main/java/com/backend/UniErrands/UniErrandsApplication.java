5ebbfdc4d13afa9690036cd1dda25fd0b9d6f07b
nikitha-56 <nikithatalluri1@gmail.com>
2025-02-27 01:02:36 +0530

    connected backend and sql ,springboot is runnig

---
+++ backend/UniErrands/UniErrands/src/main/java/com/backend/UniErrands/UniErrandsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.UniErrands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniErrandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniErrandsApplication.class, args);
+	}
+
+}
---
+++ backend/UniErrands/UniErrands/src/test/java/com/backend/UniErrands/UniErrandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.UniErrands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniErrandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
