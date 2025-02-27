8a66eaf5fd2682219794ab2b495328d186dd0f9f
nikitha-56 <nikithatalluri1@gmail.com>
2025-02-27 22:49:11 +0530

    added user details

---
+++ backend/UniErrands/UniErrands/src/main/java/com/backend/UniErrands/Controller/UserController.java
@@ -0,0 +1,39 @@
+package com.backend.UniErrands.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.UniErrands.service.UserService;
+import java.util.List;
+import com.backend.UniErrands.model.User;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+  
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+}
